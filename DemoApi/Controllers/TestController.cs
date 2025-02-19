43d8ab9522df70cdcd9f39b9e86d10029591de74
hanzalahsuriya <hanzalah@gmail.com>
2025-02-19 01:11:38 +0000

    testcontroller and open api (#8)
    * testcontroller and open api
    
    * stupid format

---
+++ DemoApi/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers
+{
+    public class MyModel
+    {
+        public required string Message { get; set; }
+        public required DateTime Date { get; set; }
+        public string Details { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello World!";
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post(MyModel model)
+        {
+            return Ok(model);
+        }
+
+    }
+}
--- DemoApi/Program.cs
+++ DemoApi/Program.cs
@@ -2,14 +2,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi().AddControllers();
 
 var app = builder.Build();
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();
